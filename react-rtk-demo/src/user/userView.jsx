2e4ee21769600d361c4e169b1cd879c511c45a63
CoorayNTL <tl685998@gmail.com>
2023-07-08 20:13:38 +0530

    use api magement with react+redux

--- react-rtk-demo/src/user/userView.jsx
+++ react-rtk-demo/src/user/userView.jsx
@@ -1,9 +1,29 @@
-
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const userView = () => {
+    const user = useSelector((state) => state.user);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(fetchUsers());
+    }, []);
+
     return (
         <>
             <h2>List of Users</h2>
+            {user.loading && <div>Loading...</div>}
+            {!user.loading && user.error ? (
+                <div> Error : {user.error}</div>
+            ) : null}
+
+            {!user.loading && user.users.length ? (
+                <ul>
+                    {user.users.map((user) => (
+                        <li key={user.id}>{user.name}</li>
+                    ))}
+                </ul>
+            ) : null}
         </>
     );
 };
